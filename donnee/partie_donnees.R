f29f7df75180532032ae207ba779a50e88596bbb
Ismail Akrim <akrim.ismail.cp1@gmail.com>
2016-04-20 10:32:09 +0000

    reorganising scripts about data model

--- donnee/partie_donnees.R
+++ donnee/partie_donnees.R
@@ -116,20 +116,3 @@ nombreToLigne <- function(vector){
 
 
 
-########################## Partie arbre decision 
-library(splitstackshape)
-library(rattle)
-set.seed(1)
-Donnees_Stra<- stratified(Migrated_Model_DATA, c("PLAN_CIBLE_CATEGO"),min(table(Migrated_Model_DATA$PLAN_CIBLE_CATEGO)))
-ind <- sample(2, nrow(Donnees_Stra), replace=TRUE, prob=c(0.7, 0.3))
-trainData <- Donnees_Stra[ind==1,]
-testData <- Donnees_Stra[ind==2,]
-Arbre <- rxDTree(PLAN_CIBLE_CATEGO ~ ANCIENETE + MMPR + AVG_OB_INT + AVG_OB_BASE + AVG_DATA_Go + AVG_APPELANTS + AVG_APPELES,data = trainData, maxDepth = 10,cp = 0.002 )
-fancyRpartPlot(rxAddInheritance(Arbre))
-##### Loading data example 
-Migrated_Model_DATA <- read.csv("C:/Users/Pr03450/Desktop/ISMAIL/donnee/Migrated_Model_DATA.txt", sep=";", na.strings=".")
-View(Migrated_Model_DATA)
-Migrated_Model_DATA$PLAN_SOURCE <- as.factor(Migrated_Model_DATA$PLAN_SOURCE)
-Migrated_Model_DATA$PLAN_CIBLE <- as.factor(Migrated_Model_DATA$PLAN_CIBLE)
-Migrated_Model_DATA$LIGNE <- nombreToLigne(Migrated_Model_DATA$LIGNE)
-View(Migrated_Model_DATA)
